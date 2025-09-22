18f376b79fb4a5b3c1610f405189cc1d7b751ef1
savagerunderscorelex <eigbekoyi@gmail.com>
2025-09-22 18:15:14 -0400

    yayyyyyyyyyyyy I finally got the fetching the input value thing to work

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Getting all of the mad lib words and initializing the submit button
 
+var heading = document.getElementById("heading");
+var input = document.getElementById("1st-noun");
+var button = document.querySelector("button");
+var inputSecond = document.getElementById("verb-ing");
+button.addEventListener("click", heyExample);
 
-
-
+function heyExample() {
+    heading.innerText = input.value;
+    heading.innerText = inputSecond.value;
+}   
 
