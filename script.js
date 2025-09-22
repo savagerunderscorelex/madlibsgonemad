47238363180a91a243e1d933ce605d0003671601
savagerunderscorelex <eigbekoyi@gmail.com>
2025-09-22 19:34:33 -0400

    added another todo (write better madlib templates), create javascript variables for all inputs for the first mad lib,

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Getting all of the mad lib words and initializing the submit button
 
+var button = document.querySelector("button"); // Submit button
+
 var heading = document.getElementById("heading");
-var input = document.getElementById("1st-noun");
-var button = document.querySelector("button");
-var inputSecond = document.getElementById("verb-ing");
-button.addEventListener("click", heyExample);
+var firstNoun = document.getElementById("1st-noun");
+var verbIng = document.getElementById("verb-ing");
+var secondNoun = document.getElementById("2nd-noun");
+var thirdNoun = document.getElementById("3rd-noun");
+var firstAdjective = document.getElementById("1st-adjective");
+var serviceJob = document.getElementById("service-job");
+var firstNumber = document.getElementById("1st-number");
+var firstColor = document.getElementById("1st-color");
+var fourthNoun = document.getElementById("4th-noun");
+var secondNumber = document.getElementById("2nd-number");
+
+
+button.addEventListener("click", heyExample); // Calls the function when the button's clicked on
 
 function heyExample() {
+    // Empty for now 
+    /*
     heading.innerText = input.value;
     heading.innerText = inputSecond.value;
+    heading.innerText = inputThird.value;
+    */
 }   
 
--- todo.md
+++ todo.md
@@ -1,8 +1,11 @@
 # To Do
 
 * Create Mad Libs selection page
-* Writer the mad libs story with blanks
-* figure out how to use js to insert user inputs into a mad lib
+* Write the mad libs story with blanks
+* ~~figure out how to use js to insert user inputs into a mad lib~~
 * Also choose fonts for the website
 * finish homepage
 * maybe figure out how to allow users to create custom links to share their mad lib results? (it might take me years to do it ngl)
+* Create website for each mad lib
+* probably branch from the main every time I have a problem that might take me 2 hours like the fetching input issue (we have branches for a reason :p)
+* write wayyy better mad lib templates (look for inspo online, or base it off one of my existing stories)
