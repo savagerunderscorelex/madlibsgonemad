20d38044d53cad76edf96d51e38d9db67765eec0
savagerunderscorelex <eigbekoyi@gmail.com>
2025-09-22 17:41:14 -0400

    tried to get input element values, deleted code from script, moved madlib 1 form out of folder

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Getting all of the mad lib words and initializing the submit button
-let button = document.getElementById("submit");
-var noun1 = document.getElementById("1st-noun");
-let verb = document.getElementById("verb-ing");
-let noun2 = document.getElementById("2nd-noun");
-let form = document.getElementById("form");
 
 
-alert(testNoun);
-form.addEventListener("submit", function() {
-    var testNoun = noun1.value;
-    
-});
 
-==
+
+
